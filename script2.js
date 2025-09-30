45332b1999bc8155db3dd2345470fc7afae4ab58
suellen255 <anasuellen31@gmail.com>
2025-09-30 12:01:16 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,19 @@
+//arays -> vetores, matrizes 
+// estrutura de dfados para armanezar um conjunto de elementos 
+
+let frutas = ["Melancia", "Manga" , "laranja"]; //array de tamanho 3
+console.log(frutas);
+console.log(frutas[1]);
+
+let números = new Array(2,8,9,10);
+console.log(números);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "Suellen";
+console.log(arrayVazio);
+
+
+//Tamanho do array -> length
+let dados = [10, 90, 30]; //tamanho  3 -> legth 3
+console.log(dados.length);
